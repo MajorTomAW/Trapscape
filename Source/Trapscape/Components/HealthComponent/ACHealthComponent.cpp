a5340ab48660a7d2486cc4f5ee99a4713f6f1ff1
MajorTomAW <tom.wrnr.private@gmail.com>
2023-04-30 21:54:38 +0200

    Added Advanced Sessions Steam

--- Source/Trapscape/Components/HealthComponent/ACHealthComponent.cpp
+++ Source/Trapscape/Components/HealthComponent/ACHealthComponent.cpp
@@ -1,34 +1,73 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "ACHealthComponent.h"
 
-// Sets default values for this component's properties
+#include "Net/UnrealNetwork.h"
+
 UACHealthComponent::UACHealthComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	SetIsReplicated(true);
 }
 
-
-// Called when the game starts
 void UACHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UACHealthComponent::OwnerDamage);
+	CurrentHealth = PlayerHealth;
 }
 
-
-// Called every frame
 void UACHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
+
+
+void UACHealthComponent::OwnerDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* EventInstigator, AActor* DamageCauser)
+{
+	if (!bIsDead)
+	{
+		CurrentHealth = FMath::Clamp(CurrentHealth - Damage, 0.0f, GetMaxHealth());
+		OnUpdateHealth.Broadcast(CurrentHealth);
+		if (CurrentHealth <= 0.0f)
+		{
+			bIsDead = true;
+			OnPlayerDeath.Broadcast();
+		}
+	}
+}
+
+void UACHealthComponent::HealPlayer(float HealValue)
+{
+	CurrentHealth = FMath::Clamp(CurrentHealth + HealValue, 0.0f, GetMaxHealth());
+	if (bIsDead) bIsDead = false;
+	OnUpdateHealth.Broadcast(CurrentHealth);
+}
+
+void UACHealthComponent::ResetHealth()
+{
+	CurrentHealth = PlayerHealth;
+	bIsDead = false;
+	OnUpdateHealth.Broadcast(CurrentHealth);
+}
+
+//Pure Function
+float UACHealthComponent::GetCurrentHealth()
+{
+	return CurrentHealth;
+}
+float UACHealthComponent::GetMaxHealth()
+{
+	return PlayerHealth;
+}
+bool UACHealthComponent::GetIsDead()
+{
+	return bIsDead;
+}
+
+//Replication
+void UACHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UACHealthComponent, CurrentHealth);
+	DOREPLIFETIME(UACHealthComponent, bIsDead);
+}
--- Source/Trapscape/Components/HealthComponent/ACHealthComponent.h
+++ Source/Trapscape/Components/HealthComponent/ACHealthComponent.h
@@ -1,11 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "ACHealthComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPlayerDeath);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnUpdateHealth, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnUpdateMaxHealth, float, NewMaxHealth);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TRAPSCAPE_API UACHealthComponent : public UActorComponent
@@ -13,16 +14,41 @@ class TRAPSCAPE_API UACHealthComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UACHealthComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
+	UPROPERTY(Replicated)
+		float CurrentHealth;
+	UPROPERTY(Replicated)
+		bool bIsDead = false;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
+	UFUNCTION()
+		void OwnerDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* EventInstigator, AActor* DamageCauser);
 
-		
+	//Values
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
+		float PlayerHealth = 100.0f;
+	
+	//Dispatchers
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FOnPlayerDeath OnPlayerDeath;
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FOnUpdateHealth OnUpdateHealth;
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FOnUpdateMaxHealth OnUpdateMaxHealth;
+	//Functions
+	UFUNCTION(BlueprintCallable)
+		void HealPlayer(float HealValue);
+	UFUNCTION(BlueprintCallable)
+		void ResetHealth();
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		float GetCurrentHealth();
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		float GetMaxHealth();
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		bool GetIsDead();
 };
